692a34eac5b21e37d9da628d91689dcf893257a0
Ansar Ismagilov <ansar89kab@bk.ru>
2022-12-22 19:50:56 +0100

    commit 5 - name change last

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String hahaha = "gogirian";
+        String hahaha = "gogiriandfg";
         System.out.println("main");
         System.out.println("main");
         System.out.println("main");
