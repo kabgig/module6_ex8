97d801ff90586b234012705d59f09f1ef80b7b8e
Ansar Ismagilov <ansar89kab@bk.ru>
2022-12-22 19:34:59 +0100

    new_branch commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        String name = "gogi";
         System.out.println("main");
         System.out.println("main");
         System.out.println("main");
