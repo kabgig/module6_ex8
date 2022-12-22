156e80bcb8e15797b17d2de6ca1dd84491d5672f
Ansar Ismagilov <ansar89kab@bk.ru>
2022-12-22 19:44:44 +0100

    change branch_1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String hahaha = "gogi";
+        String hahaha = "gogirian";
         System.out.println("main");
         System.out.println("main");
         System.out.println("main");
